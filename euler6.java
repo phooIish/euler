b4018190c74f0c911d368c3c53e0e513009a8d87
Jerric Cayas <jerric.cayas@gmail.com>
2017-07-12 08:59:48 -0700

    triangular numbers

---
+++ euler6.java
@@ -0,0 +1,19 @@
+public class euler6 {
+    public static void main(String[] args) {
+        int sq_sum = squareOfsum(100) * squareOfsum(100);
+        int ans = sq_sum - sumOfsquares(100);
+        System.out.println(ans);
+    }
+    static int squareOfsum(int x){
+        if(x == 1){
+            return 1;
+        }
+        return x + squareOfsum(x - 1);
+    }
+    static int sumOfsquares(int y){
+        if(y == 1){
+            return 1;
+        }
+        return (y*y) + sumOfsquares(y - 1);
+    }
+}
